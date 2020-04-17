91838edbab50deb06ce1edf1fbc08dd38c885718
samara <samduarte1234@gmail.com>
2020-04-17 19:42:25 -0300

    primeira parte do curso

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { ReplacePipe } from './pipe/replace.pipe';
+import { StarComponent } from './star/star.component';
+import { CourseListComponent } from './courses/course-list.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,39 @@
+import { CourseSevice } from './courses.servise';
+import { Component } from '@angular/core';
+import { Course } from './course';
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html'
+})
+export class CourseListComponent {
+
+  filteredCourses: Course[] = [];
+
+  courses: Course[] = [];
+
+  filterBy: string;
+
+  constructor(private courseSevice: CourseSevice){
+
+  }
+
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnInit(): void {
+    this.courses = this.courseSevice.retrieveAll();
+    this.filteredCourses = this.courses;
+  }
+
+  set filter(value: string){
+  this.filterBy = value;
+  // tslint:disable-next-line: max-line-length
+  this.filteredCourses = this.courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this.filterBy.toLocaleLowerCase()) > -1);
+
+  }
+
+  get filter(){
+    return this.filterBy;
+  }
+
+
+}
